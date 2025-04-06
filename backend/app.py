0814b045992022c5074cda86928c11f28e2c465f
Abhishek Chaurasiya <abhiabckchaurasiya@gmail.com>
2025-04-06 19:14:27 +0530

    Changement's in some project folder structure, and some enhancements in master temp

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,14 @@
-from flask import Flask, request, url_for
+from flask import Flask, request, url_for, render_template
 from html import escape
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", title="Home Page")
+
+@app.route('/Login')
+def login_menu():
+    return render_template('LoginMenu.html')
